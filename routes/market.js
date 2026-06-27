57bea4a2a12430d1b7d0d857f30c34880d267ad2
Demo User <rahsidurrahmanshawon@gmail.com>
2026-06-27 18:46:17 +0600

    feat: secure user history, threshold creation and deletion routes using JWT protectionRoute middleware

--- routes/market.js
+++ routes/market.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const marketController = require('../controllers/marketControllar');
+const protectRoute = require('../middleware/authMiddleware');
 
-router.get('/history/all', marketController.getAlertHistory);
+
+router.get('/history/all', protectRoute, marketController.getAlertHistory);
 
 router.get('/:coin', marketController.getMarketData);
 
-router.post('/threshold', marketController.calculateThreshold);
+router.post('/threshold', protectRoute, marketController.calculateThreshold);
 
-router.delete('/alert/:id', marketController.deleteAlert);
+router.delete('/alert/:id', protectRoute, marketController.deleteAlert);
 
 module.exports = router;
